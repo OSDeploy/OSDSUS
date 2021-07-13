abd5a43c4cc8511008ccbbba69b1c917e839c9e2
David Segura <david@segura.org>
2021-07-13 13:53:16 -0500

    21.7.13.1

--- Catalogs/CleanupXml.ps1
+++ Catalogs/CleanupXml.ps1
@@ -1,7 +1,7 @@
 ﻿$XmlFiles = Get-ChildItem -Path $PSScriptRoot *.xml | Where-Object {$_.Name -Match 'Windows'} | Select-Object BaseName, Name, DirectoryName, FullName
 
 foreach ($XmlFile in $XmlFiles) {
-    $XmlContent = Import-Clixml $XmlFile.FullName | Sort-Object Hash | Out-GridView -PassThru -Title "$($XmlFile.FullName)"
+    $XmlContent = Import-Clixml $XmlFile.FullName | ? IsSuperseded | Sort-Object Hash | Out-GridView -PassThru -Title "$($XmlFile.FullName)"
     $XmlContent | Export-Clixml "$PSScriptRoot\$($XmlFile.BaseName) $(Get-Random).xml"
     Pause
 }
