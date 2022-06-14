6b6465052d9fd34b3426bcd2955d061dab988a37
David Segura <david@segura.org>
2022-06-14 13:27:36 -0500

    22.6.14.1

--- Catalogs/Build-Updates.md.ps1
+++ Catalogs/Build-Updates.md.ps1
@@ -1,7 +1,8 @@
+Import-Module OSDSUS -Force
 $AllOSDUpdates = @()
 $AllUpdateCatalogs = Get-ChildItem -Path "$PSScriptRoot\*" -Include '*.xml' -Recurse
 foreach ($UpdateCatalog in $AllUpdateCatalogs) {$AllOSDUpdates += Import-Clixml -Path "$($UpdateCatalog.FullName)"}
 
 $AllOSDUpdates = $AllOSDUpdates | Select-Object -Property * | Sort-Object -Property Title -Unique | Sort-Object CreationDate -Descending #| Out-GridView -PassThru -Title "All OSDUpdates"
 Write-Host ""
-$AllOSDUpdates | Select-Object -Property CreationDate, KBNumber, Title | Sort @{Expression = {$_.CreationDate}; Ascending = $false}, KBNumber, Title | Out-File C:\Users\osdcloud\Documents\GitHub\OSDSUS\UPDATES.md
+$AllOSDUpdates | Select-Object -Property CreationDate, KBNumber, Title | Sort @{Expression = {$_.CreationDate}; Ascending = $false}, KBNumber, Title | Out-File D:\GitHub\MyModules\OSDSUS\UPDATES.md
